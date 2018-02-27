80058b1b510d01942fe631b4a8dc4c76c5593547
sahlinstefan <34234973+sahlinstefan@users.noreply.github.com>
2018-02-27 11:13:20 +0100

    Added datamodel and Admin on startup
    Configurated file startup.cs

--- MovieShop/Startup.cs
+++ MovieShop/Startup.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
+using MovieShop.Models;
+using Microsoft.AspNet.Identity;
 
 [assembly: OwinStartupAttribute(typeof(MovieShop.Startup))]
 namespace MovieShop
@@ -9,6 +13,47 @@ namespace MovieShop
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
         }
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User 
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // create admin roole
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //create default admin user 
+                var user = new ApplicationUser();
+                user.UserName = "AdminUser";
+                user.Email = "adminguser@yahoo.com";
+
+                string userPWD = "@Lexicon11";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add Stefan to Role Admin
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+
+
+         
+        }
+
     }
 }
+
