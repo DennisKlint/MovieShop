5391ecc9c574a4264e9ae6250b1c9ba120c066a0
sahlinstefan <34234973+sahlinstefan@users.noreply.github.com>
2018-02-28 09:57:24 +0100

    Edited identitymodel tables
    Added forignkeys updated fields in products table

--- MovieShop/Models/IdentityModels.cs
+++ MovieShop/Models/IdentityModels.cs
@@ -9,6 +9,9 @@ namespace MovieShop.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
